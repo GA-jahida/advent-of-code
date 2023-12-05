7f7b3eca13f04370851956f989bdbccf8aa0cd3a
jxa669 <jxa669@student.bham.ac.uk>
2023-12-05 15:42:12 +0000

    update

--- 2023/day5.py
+++ 2023/day5.py
@@ -1,3 +1,4 @@
+import math
 def get_min_location(lines, start_array, seeds):
     seed_to_soil = [[] for x in range(start_array[0], start_array[1]-2)]
     for i in range(start_array[0], start_array[1]-2):
@@ -28,7 +29,8 @@ def get_min_location(lines, start_array, seeds):
         humid_to_location[i - start_array[6]] = lines[i].strip().split(" ")
 
     min_location = None
-    for seed in seeds:
+    min_seed = None
+    for index, seed in enumerate(seeds):
         soil = map_number(seed_to_soil, int(seed))
         fertilizer = map_number(soil_to_fertilizer, soil)
         water = map_number(fertilizer_to_water, fertilizer)
@@ -36,24 +38,23 @@ def get_min_location(lines, start_array, seeds):
         temp = map_number(light_to_temp, light)
         humid = map_number(temp_to_humid, temp)
         location = map_number(humid_to_location, humid)
-        print(location)
+
         if min_location is None or location < min_location:
             min_location = location
-    return min_location
+            min_seed = seed
+    return min_location, min_seed
 
 
 def map_number(lists, number):
     for list in lists:
         if int(list[1]) + int(list[2]) > number >= int(list[1]):
-            # print(number + int(list[0]) - int(list[1]))
             return number + int(list[0]) - int(list[1])
-    # print(number)
     return number
 
 def part1(lines):
     seeds = lines[0].replace("seeds: ", "").strip().split(" ")
+    start_array = [3, 7, 12, 18, 22, 27, 31]
     start_array = [3, 20, 35, 68, 92, 127, 173]
-    # start_array = [3, 7, 12, 18, 22, 27, 31]
     return get_min_location(lines, start_array, seeds)
 
 
@@ -62,15 +63,21 @@ def part2(lines):
     start_array = [3, 20, 35, 68, 92, 127, 173]
     seed_ranges = lines[0].replace("seeds: ", "").strip().split(" ")
     min_location = None
+    seeds = []
+
     for i in range(0, len(seed_ranges), 2):
-        seeds = []
-        for j in range(int(seed_ranges[i]), int(seed_ranges[i]) + int(seed_ranges[i+1])):
-            seeds.append(j)
-        min_current = get_min_location(lines, start_array, seeds)
-        if min_location is None or min_current < min_location:
-            min_location = min_current
+        seeds += [x for x in range(int(seed_ranges[i]), int(seed_ranges[i]) + int(seed_ranges[i+1]), math.isqrt(int(seed_ranges[i+1])))]
+    
+    estimated_search_min = get_min_location(lines, start_array, seeds)
+    min_location = None
+    seeds = []
+    for i in range(0, len(seed_ranges), 2):
+        if int(seed_ranges[i]) < estimated_search_min[1] < int(seed_ranges[i]) + int(seed_ranges[i + 1]):
+            for j in range(int(seed_ranges[i]), int(seed_ranges[i]) + int(seed_ranges[i+1]), 1):
+                if abs(estimated_search_min[1] - j) < 10000:
+                    seeds.append(j)
     return get_min_location(lines, start_array, seeds)
 
 lines = open("./inputs/day5-input.txt", "r").readlines()
-# print("Q1:", part1(lines))
+print("Q1:", part1(lines))
 print("Q2:", part2(lines))
